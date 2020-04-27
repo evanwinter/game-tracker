b5b0fa435a6d5423cd667b913b19ef532e57431b
Evan Winter <evan.winter@brookfieldpropertiesretail.com>
2020-04-27 07:30:56 -0500

    add ActionButtons

---
+++ src/components/ActionButtons/index.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+export const BackButton = () => {
+  return (
+    <button className="BackButton">
+      Back
+    </button>
+  )
+}
+
+export const NextButton = () => {
+  return (
+    <button className="NextButton">
+      Next
+    </button>
+  )
+}
+
+export const AddButton = () => {
+  return (
+    <button className="AddButton">
+      Add
+    </button>
+  )
+}
