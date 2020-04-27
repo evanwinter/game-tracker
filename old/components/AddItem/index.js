81c19421202b48b97c414909f5fd96fd6a54014f
Evan Winter <evan.winter@brookfieldpropertiesretail.com>
2020-04-26 22:05:09 -0500

    remote old files

--- old/components/AddItem/index.js
+++
@@ -1,6 +0,0 @@
-import AddItemButton from "./AddItemButton"
-import AddItemForm from "./AddItemForm"
-
-import "./styles.scss"
-
-export { AddItemButton, AddItemForm }
--- old/components/Loader/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import "./styles.scss"
-
-const Loader = () => {
-	return <div className="Loader"></div>
-}
-
-export default Loader
